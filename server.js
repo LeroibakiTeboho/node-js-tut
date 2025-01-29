7b77cd2f373b8e259a63ec2c0145b7f0a66d7c0d
Teboho <leroibakiteboho@gmail.com>
2025-01-29 13:39:51 +0200

    routing

--- server.js
+++ server.js
@@ -5,10 +5,10 @@ const cors = require("cors");
 const { logger } = require("./middleware/logEvents");
 const errorHandler = require("./middleware/errorHandler");
 
-// Define the port number for the server
+//* Define the port number for the server
 const PORT = process.env.PORT || 3500;
 
-// custom middleware logger
+//* custom middleware logger
 app.use(logger);
 
 const whiteList = [
@@ -26,7 +26,7 @@ const corsOptions = {
   },
   optionsSuccessStatus: 200,
 };
-// Enable CORS (Cross-Origin Resource Sharing) for all requests
+//* Enable CORS (Cross-Origin Resource Sharing) for all requests
 app.use(cors(corsOptions));
 
 //* built-in middleware to handle urlencoded data
@@ -34,67 +34,20 @@ app.use(cors(corsOptions));
 //* 'content-type': 'application/x-www-form-urlencoded'
 app.use(express.urlencoded({ extended: true }));
 
-// built-in middleware to handle JSON data
+//* built-in middleware to handle JSON data
 app.use(express.json());
 
-// server static files from the "public" directory
+//* server static files from the "public" directory
 app.use(express.static(path.join(__dirname, "/public")));
+app.use("/subdir", express.static(path.join(__dirname, "/public")));
 
-// Serve static files from the "public" directory
-app.get("^/$|/index(.html)?", (req, res) => {
-  // Serve the index.html file from the "views" directory
-  res.sendFile(path.join(__dirname, "views", "index.html"));
-});
-
-app.get("^/$|/data(.json)?", (req, res) => {
-  // Serve the index.html file from the "views" directory
-  res.sendFile(path.join(__dirname, "data", "data.json"));
-});
-
-// Additional routes can be added here...
-app.get("/new-page(.html)?", (req, res) => {
-  // Serve the new-page.html file from the "views" directory
-  res.sendFile(path.join(__dirname, "views", "new-page.html"));
-});
-
-app.get("/old-page(.html)?", (req, res) => {
-  // Serve the new-page.html file from the "views" directory
-  res.redirect(301, "/new-page.html");
-});
-
-// Route handler
-app.get(
-  "/hello(.html)?",
-  (req, res, next) => {
-    console.log("attempted to load hello.html");
-    next();
-  },
-  (req, res) => {
-    res.send("Hello World!");
-  }
-);
-
-// chaining route handlers
-
-const one = (req, res, next) => {
-  console.log("one");
-  next();
-};
-
-const two = (req, res, next) => {
-  console.log("two");
-  next();
-};
-
-const three = (req, res) => {
-  console.log("three");
-  res.send("finished!");
-};
-
-app.get("/chain(.html)?", [one, two, three]);
+//* Define routes
+app.use("/", require("./routes/root"));
+app.use("/subdir", require("./routes/subdir"));
+app.use("/employees", require("./routes/api/employees"));
 
 app.all("*", (req, res) => {
-  // Send a 404 response for all other requests
+  //* Send a 404 response for all other requests
   res.status(404);
 
   // Send a custom 404 error message based on the requested format
@@ -109,5 +62,5 @@ app.all("*", (req, res) => {
 
 app.use(errorHandler);
 
-// Start the server
+//* Start the server
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
